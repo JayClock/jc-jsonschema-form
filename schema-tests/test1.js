f16c38cf1aac040d4d26ca21f404e29c2d6f8236
钟杰 <891853602@qq.com>
2021-04-25 07:14:50 +0800

    自定义校验规则

--- schema-tests/test1.js
+++ schema-tests/test1.js
@@ -1,23 +1,28 @@
 // Node.js require:
 const Ajv = require('ajv')
+const addFormats = require('ajv-formats')
 
 const ajv = new Ajv() // options can be passed, e.g. {allErrors: true}
+// 引入ajv校验规则
+addFormats(ajv)
+// 自定义校验规则
+ajv.addFormat('test', (data) => {
+  return data === 'test'
+})
 
 const schema = {
   type: 'object',
   properties: {
     name: {
       type: 'string',
-      minLength: 10,
+      format: 'test',
+      //   minLength: 2,
     },
     age: {
       type: 'number',
     },
     pets: {
       type: 'array',
-      items: {
-        type: 'string',
-      },
     },
     isWorker: {
       type: 'boolean',
@@ -26,5 +31,7 @@ const schema = {
 }
 
 const validate = ajv.compile(schema)
-const valid = validate(data)
+const valid = validate({
+  name: 'jayclock',
+})
 if (!valid) console.log(validate.errors)
