44ec48cb1401ad61d74de1934d3b913974aabb8f
钟杰 <891853602@qq.com>
2021-04-25 21:25:19 +0800

    demo完善

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <monaco-editor
+    class="editor"
+    :code="code"
+    :onChange="handleCodeChange"
+    title="Schema"
+  ></monaco-editor>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
-
+import { defineComponent, ref, Ref } from "vue";
+import MonacoEditor from "./components/MonacoEditor";
 export default defineComponent({
   name: "App",
   components: {
-    HelloWorld,
+    MonacoEditor,
+  },
+  setup() {
+    // schema对象
+    const schema = {
+      type: "string",
+    };
+    // 对象转字符串
+    const toJson = (data: any) => {
+      return JSON.stringify(data, null, 2);
+    };
+
+    const schemaRef: Ref<any> = ref(schema);
+    // 在表单值有任何变化的时候会触发该回调方法，并把新的值进行返回
+    const handleCodeChange = (code: string) => {
+      let schema: any;
+      try {
+        schema = JSON.parse(code);
+      } catch (e) {
+        console.log(e);
+      }
+      schemaRef.value = schema;
+    };
+    return {
+      handleCodeChange,
+      code: toJson(schemaRef.value),
+    };
   },
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.editor {
+  min-height: 400px;
 }
 </style>
