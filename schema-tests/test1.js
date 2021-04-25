8377fb36195e18c0b8591343863ae3218841c27c
钟杰 <891853602@qq.com>
2021-04-25 18:49:56 +0800

    自定义错误信息

--- schema-tests/test1.js
+++ schema-tests/test1.js
@@ -2,13 +2,16 @@
 const Ajv = require('ajv')
 const addFormats = require('ajv-formats')
 const addKeywords = require('ajv-keywords')
-const localize = require('ajv-i18n')
+// const localize = require('ajv-i18n')
+const addErrors = require('ajv-errors')
 
-const ajv = new Ajv()
+const ajv = new Ajv({ allErrors: true })
 // 引入ajv校验规则
 addFormats(ajv)
 // 引入ajv关键字
 addKeywords(ajv)
+// 引入ajv错误信息
+addErrors(ajv)
 // 自定义校验规则
 // ajv.addFormat('test', (data) => {
 //   return data === 'test'
@@ -34,9 +37,14 @@ const schema = {
   properties: {
     name: {
       type: 'string',
-      format: 'email',
-      test: '1',
-      //   minLength: 2,
+      // format: 'email',
+      // test: '1',
+      minLength: 10,
+      // 自定义错误信息
+      errorMessage: {
+        type: '必须是字符串',
+        minLength: '长度必须小于10',
+      },
     },
     age: {
       type: 'number',
@@ -56,6 +64,6 @@ const valid = validate({
 })
 
 if (!valid) {
-  localize.zh(validate.errors)
+  // localize.zh(validate.errors)
   console.log(validate.errors)
 }
