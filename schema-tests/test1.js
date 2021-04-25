f0435f3a08adf2f47521fdcf2f26da8b6d5fa754
钟杰 <891853602@qq.com>
2021-04-25 17:40:23 +0800

    自定义关键字

--- schema-tests/test1.js
+++ schema-tests/test1.js
@@ -1,13 +1,31 @@
 // Node.js require:
 const Ajv = require('ajv')
 const addFormats = require('ajv-formats')
+const addKeywords = require('ajv-keywords')
 
-const ajv = new Ajv() // options can be passed, e.g. {allErrors: true}
+const ajv = new Ajv()
 // 引入ajv校验规则
 addFormats(ajv)
+// 引入ajv关键字
+addKeywords(ajv)
 // 自定义校验规则
-ajv.addFormat('test', (data) => {
-  return data === 'test'
+// ajv.addFormat('test', (data) => {
+//   return data === 'test'
+// })
+// 自定义关键字
+ajv.addKeyword({
+  keyword: 'test',
+  // compile: (schema, praentSchema) => {
+  //   return () => true
+  // },
+  // metaSchema: {
+  //   type: 'string',
+  // },
+  macro: (schema, praentSchema) => {
+    return {
+      minLength: 10,
+    }
+  },
 })
 
 const schema = {
@@ -15,7 +33,8 @@ const schema = {
   properties: {
     name: {
       type: 'string',
-      format: 'test',
+      format: 'email',
+      test: '1',
       //   minLength: 2,
     },
     age: {
@@ -34,4 +53,5 @@ const validate = ajv.compile(schema)
 const valid = validate({
   name: 'jayclock',
 })
+
 if (!valid) console.log(validate.errors)
