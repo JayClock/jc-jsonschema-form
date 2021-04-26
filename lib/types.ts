6d349279e3f77222961ff51cb7e998a24adb3708
钟杰 <891853602@qq.com>
2021-04-26 20:32:41 +0800

    添加依赖和工具类

--- lib/types.ts
+++ lib/types.ts
@@ -1,4 +1,4 @@
-import { PropType } from "@vue/runtime-core";
+import { PropType } from "vue";
 
 export enum SchemaTypes {
   "NUMBER" = "number",
@@ -6,43 +6,59 @@ export enum SchemaTypes {
   "STRING" = "string",
   "OBJECT" = "object",
   "ARRAY" = "array",
-  "BOOLEAN" = "boolean",
+  "BOOLEAN" = "boolean"
 }
 
 type SchemaRef = { $ref: string };
 
 // type Schema = any
 export interface Schema {
-  type: SchemaTypes | string;
+  type?: SchemaTypes | string;
   const?: any;
   format?: string;
+
+  title?: string;
   default?: any;
+
   properties?: {
     [key: string]: Schema | { $ref: string };
   };
   items?: Schema | Schema[] | SchemaRef;
+  uniqueItems?: any;
   dependencies?: {
     [key: string]: string[] | Schema | SchemaRef;
   };
   oneOf?: Schema[];
+  anyOf?: Schema[];
+  allOf?: Schema[];
+  // TODO: uiSchema
   // vjsf?: VueJsonSchemaConfig
   required?: string[];
   enum?: any[];
+  enumNames?: any[];
   enumKeyValue?: any[];
   additionalProperties?: any;
   additionalItems?: Schema;
+
+  minLength?: number;
+  maxLength?: number;
+  minimun?: number;
+  maximum?: number;
+  multipleOf?: number;
+  exclusiveMaximum?: number;
+  exclusiveMinimum?: number;
 }
 
 export const FiledPropsDefine = {
   schema: {
     type: Object as PropType<Schema>,
-    required: true,
+    required: true
   },
   value: {
-    required: true,
+    required: true
   },
   onChange: {
     type: Function as PropType<(v: any) => void>,
-    required: true,
-  },
+    required: true
+  }
 } as const;
