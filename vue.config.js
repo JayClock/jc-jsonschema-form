7b756b79f86fa4a421c78ce434aad45dc8551b25
钟杰 <891853602@qq.com>
2021-06-14 10:44:23 +0800

    拆分主题打包

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,13 @@
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin') // 清除编辑器报错
 const CircularDependencyPlugin = require('circular-dependency-plugin') // 防止模块循环引用
 
+const isLib = process.env.TYPE === 'lib'
+
 module.exports = {
   chainWebpack(config) {
-    config.plugin('monaco').use(new MonacoWebpackPlugin())
+    if (!isLib) {
+      config.plugin('monaco').use(new MonacoWebpackPlugin())
+    }
     config.plugin('circular').use(new CircularDependencyPlugin())
   },
 }
