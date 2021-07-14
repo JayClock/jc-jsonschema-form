edb36bbac94afca7c4142dda3871df9449a66cb5
钟杰 <891853602@qq.com>
2021-07-15 07:53:53 +0800

    迁移TextWidget

---
+++ lib/fields/StringField.tsx
@@ -0,0 +1,29 @@
+import { defineComponent } from 'vue'
+
+import { FieldProps, CommonWidgetNames } from '../types'
+import { getWidget } from '../theme'
+
+export default defineComponent({
+  name: 'StringFeild',
+  props: FieldProps,
+  setup(props) {
+    const handleChange = (v: string) => {
+      // console.log(e)
+      props.onChange(v)
+    }
+
+    const TextWidgetRef = getWidget(CommonWidgetNames.TextWidget)
+
+    return () => {
+      const { schema, rootSchema, ...rest } = props
+
+      const TextWidget = TextWidgetRef.value
+
+      return <TextWidget {...rest} onChange={handleChange} />
+
+      // return (
+      //   <input type="text" value={props.value as any} onInput={handleChange} />
+      // )
+    }
+  },
+})
